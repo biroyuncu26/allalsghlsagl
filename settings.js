af7c64679d9d34cefef321b1331e48d2980d0ae5
biroyuncu26 <34104087+biroyuncu26@users.noreply.github.com>
2021-12-09 21:10:31 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,38 @@
+module.exports = {
+    bot: {
+        botOwner: [""],
+        botToken: "ODk0MTk4NTUxMzc3MjE1NDg5.YVmhTA.ZTA2MPDNGfkMC8UC4oMF18c3N5w",
+        botPrefix: ".",
+        botStatus: "Jrexssa"
+
+
+    },
+    guild: {
+        id: "913844611213045791",
+        tag: "✧",
+        unTag: "•"
+    },
+
+    roles: {
+        manRoles: ["913867406458372106","913868039164936252"],
+        womanRoles: ["913867517322203138","913868055996674130"],
+        unregisterRoles: ["913867569495146536"],
+        registerStaff: "913868777542795275",
+        Suspecious: "913875831431000104",
+        vipRole: "913868384523931689",
+        boosterRole: "",
+        botCommands: "",
+        tagRole: "914095827264360469"
+    },
+    channel: {
+        rules: "913872235603849277",
+        botVoice: "913885698568900748",
+        registerChat: "913870641600557076"
+    },
+    emojis: {
+        yes: "914857875615133726",
+        no: "914857878203019284",
+        man: "914859683943182386", // erkek ve kız emoji ıd
+        woman: "914859684274532372"
+    }
+};
